ed0bb1914fe4a88517ef92d40a0f0320093823d8
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-20 23:22:14 -0800

    fix post test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,24 +1,13 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get posts_new_url
+  test 'should get new' do
+    get new_post_path
     assert_response :success
   end
 
-  test "should get index" do
-    get posts_index_url
+  test 'should get index' do
+    get posts_path
     assert_response :success
   end
-
-  test "should get create" do
-    get posts_create_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get posts_show_url
-    assert_response :success
-  end
-
 end
