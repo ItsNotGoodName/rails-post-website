1f74e1558ff59edbe5cf8daf840ecfd39173ee42
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-20 22:46:36 -0800

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 64 }
+  validates :body, presence: true, length: { minimum: 3, maximum: 1024 }
 end
