668e822e82b2ec0a7c1a7218486ff16dbb8d8699
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-20 23:23:40 -0800

    Wrong order

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test 'full title helper' do
-    assert_equal full_title, 'Post Website'
-    assert_equal full_title('This is a Title'), 'This is a Title'
+    assert_equal 'Post Website', full_title
+    assert_equal  'This is a Title', full_title('This is a Title')
   end
 end
