714d63bd9f2e364d8c8fe2485bcedc72f356cbbf
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-22 22:26:35 -0800

    fix

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,10 +1,9 @@
 module SessionHelper
-  def log_in(user)
+  def log_in(_user)
     session[:user_id] = user_id
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= user.find_by(id: session[:user_id])
-    end
+    @current_user ||= user.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
