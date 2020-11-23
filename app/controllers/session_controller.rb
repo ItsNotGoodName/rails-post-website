d647cad9b2109c53712aa36ef32b9eb756bd8dda
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-22 22:18:58 -0800

    login flash messages

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,11 +2,11 @@ class SessionController < ApplicationController
   def new; end
 
   def create
-    debugger
     user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
       redirect_to root_path
     else
+      flash.now[:danger] = 'Invalid credentials'
       render 'new'
     end
   end
