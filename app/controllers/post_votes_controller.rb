26b2e13374ad8eb6d88bcbafe540c214dec71a1c
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-24 14:33:46 -0800

    Frontend upvote colors

---
+++ app/controllers/post_votes_controller.rb
@@ -0,0 +1,10 @@
+class PostVotesController < ApplicationController
+  include SessionHelper
+  before_action :require_login
+
+  def create
+  end
+
+  def delete
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,18 @@ class PostsController < ApplicationController
   def index
     current_page = current_page_params
 
-    posts_plus_one = Post.order(created_at: :desc).offset(current_page * POST_PER_PAGE).limit(POST_PER_PAGE + 1)
-
+    if logged_in?
+      posts_plus_one = Post
+                       .order(created_at: :desc)
+                       .offset(current_page * POST_PER_PAGE)
+                       .limit(POST_PER_PAGE + 1)
+                       .select('posts .*, post_votes.upvote as upvote')
+                       .joins("LEFT JOIN post_votes ON post_votes .user_id = #{current_user.id} AND post_votes.post_id = posts.id")
+    else
+      posts_plus_one = Post
+                       .order(created_at: :desc)
+                       .offset(current_page * POST_PER_PAGE)
+    end
     has_next = true
 
     if posts_plus_one.length <= POST_PER_PAGE
