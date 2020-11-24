110fde21e367d2fc88bfddcb2aa570fccc8b6c58
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-23 20:45:59 -0800

    vote form

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts, only: %i[index new create show]
   resources :users, only: %i[show]
+  resources :post_votes, only: %i[create delete]
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
+  get '/logout', to: 'session#destroy'
   delete '/logout', to: 'session#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
