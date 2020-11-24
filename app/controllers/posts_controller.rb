6d27ca1f57f05206d1d0f08b11a444b76e39b32e
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-23 18:13:12 -0800

    Add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,31 @@ class PostsController < ApplicationController
   include SessionHelper
   before_action :require_login, except: %i[index show]
 
+  POST_PER_PAGE = 10
+
   def new
-    @post =  Post.new
+    @post = Post.new
   end
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    current_page = current_page_params
+
+    posts_plus_one = Post.order(created_at: :desc).offset(current_page * POST_PER_PAGE).limit(POST_PER_PAGE + 1)
+
+    has_next = true
+
+    if posts_plus_one.length == POST_PER_PAGE
+      @posts = posts_plus_one
+      has_next = false
+    else
+      @posts = posts_plus_one.slice(0, POST_PER_PAGE)
+    end
+
+    @page = {
+      prev?: current_page > 0,
+      current_page: current_page,
+      next?: has_next
+    }
   end
 
   def create
@@ -24,6 +43,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def current_page_params
+    params.fetch(:page, 0).to_i
+  end
 
   def post_params
     params.require(:post).permit(
