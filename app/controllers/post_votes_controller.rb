0373afb0582a89e623957b2d06fc74770fc335c3
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-26 23:25:43 -0800

    make vote_on_post function

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -1,31 +1,17 @@
 class PostVotesController < ApplicationController
   include SessionHelper
+  include PostVotesHelper
   before_action :require_login
 
   def vote
-    post_id = params[:post_id]
+    post = Post.find(params[:post_id])
     upvote = params[:upvote].to_i == 1
-    user = current_user
-    post_vote = PostVote.find_by(post_id: post_id, user_id: user.id)
+    vote_on_post current_user, post, upvote
 
-    if post_vote.nil?
-      # New voter
-      PostVote.new(user_id: user.id, post_id: post_id, upvote: upvote).save
-      upvote ? Post.find(post_id).increment!(:vote) : Post.find(post_id).decrement!(:vote)
-    elsif post_vote.upvote == upvote
-      # Delete vote
-      post_vote.destroy
-      upvote ? Post.find(post_id).decrement!(:vote) : Post.find(post_id).increment!(:vote)
-    else
-      # Change vote
-      post_vote.upvote = upvote
-      post_vote.save
-      upvote ? Post.find(post_id).increment!(:vote, 2) : Post.find(post_id).decrement!(:vote, 2)
-    end
     if params[:goto].nil?
       redirect_back fallback_location: root_path
     else
-      redirect_to "#{params[:goto]}##{post_id}"
+      redirect_to "#{params[:goto]}##{post.id}"
     end
   end
 end
--- app/helpers/post_votes_helper.rb
+++ app/helpers/post_votes_helper.rb
@@ -1,2 +1,20 @@
 module PostVotesHelper
+  def vote_on_post(user, post, newvote)
+    post_vote = PostVote.find_by(post_id: post.id, user_id: user.id)
+
+    if post_vote.nil?
+      # New voter
+      PostVote.new(user_id: user.id, post_id: post.id, upvote: newvote).save
+      post.increment!(:vote, newvote ? 1 : -1)
+    elsif post_vote.upvote == newvote
+      # Delete vote
+      post_vote.destroy
+      post.increment!(:vote, newvote ? -1 : 1)
+    else
+      # Change vote
+      post_vote.upvote = newvote
+      post_vote.save
+      post.increment!(:vote, newvote ? 2 : -2)
+    end
+  end
 end
