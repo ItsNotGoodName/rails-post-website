94b44053d16c15370173694f3206114da011b896
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 19:11:56 -0800

    Change maxes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 3, maximum: 64}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :body, presence: true, length: {minimum: 3, maximum: 1024}
   belongs_to :user
   has_many :votes, as: :voteable, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { self.name = name.downcase }
-  validates :name, presence: true, uniqueness: true, length: {maximum: 32}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+  validates :password, presence: true, length: {minimum: 6, maximum: 30}
   has_secure_password
   has_many :posts
   has_many :votes, as: :voteable
