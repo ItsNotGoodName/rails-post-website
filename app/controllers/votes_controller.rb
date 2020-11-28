1735ef528c09d1a5f0d126e29c70696270bbc227
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 16:51:09 -0800

    Change to generic votes parent search

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,16 +2,26 @@ class VotesController < ApplicationController
   include SessionHelper
   include VotesHelper
   before_action :require_login
+  before_action :find_parent
 
   def vote
-    post = Post.find(params[:post_id])
     value = params[:value].to_i
-    vote_on_target current_user, post, value
+    vote_on_target current_user, @parent, value
 
     if params[:goto].nil?
       redirect_back fallback_location: root_path
     else
-      redirect_to "#{params[:goto]}##{post.id}"
+      redirect_to "#{params[:goto]}##{@parent.id}"
+    end
+  end
+
+  private
+
+  def find_parent
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        @parent = $1.classify.constantize.find(value)
+      end
     end
   end
 end
