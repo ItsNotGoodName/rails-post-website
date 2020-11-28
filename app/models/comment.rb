3fc269423ee111c0f738a79d0103fcb9650c9597
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 22:51:12 -0800

    Add hash many votes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   validates :body, presence: true, length: {minimum: 3, maximum: 1024}
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+  has_many :votes, as: :voteable, dependent: :destroy
 end
