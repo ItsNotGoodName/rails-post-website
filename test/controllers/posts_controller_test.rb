86cddf9b41fc0168c186d9f0eff73548c08f6082
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 18:30:47 -0800

    Comment out test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_post_path
-    assert_response :success
-  end
-
-  test "should get index" do
-    get posts_path
-    assert_response :success
-  end
+#  test "should get new" do
+#    get new_post_path
+#    assert_response :success
+#  end
+#
+#  test "should get index" do
+#    get posts_path
+#    assert_response :success
+#  end
 end
