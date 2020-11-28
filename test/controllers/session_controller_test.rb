129c0dd604508abb5960e6e8e50297d1fd665f12
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 18:54:05 -0800

    Add tests

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,18 +1,9 @@
 require "test_helper"
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
-#  test "should get new" do
-#    get session_new_url
-#    assert_response :success
-#  end
-#
-#  test "should get create" do
-#    get session_create_url
-#    assert_response :success
-#  end
-#
-#  test "should get destroy" do
-#    get session_destroy_url
-#    assert_response :success
-#  end
+  test "should get login" do
+    get login_path
+    assert_response :success
+    assert_select "title", "Login"
+  end
 end
