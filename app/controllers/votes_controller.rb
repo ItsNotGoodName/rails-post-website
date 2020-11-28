5004cdde413aa08936010303565bbf7855254239
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-27 17:05:35 -0800

    Change find_votable

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,26 +2,23 @@ class VotesController < ApplicationController
   include SessionHelper
   include VotesHelper
   before_action :require_login
-  before_action :find_parent
+  before_action :find_votable
 
   def vote
     value = params[:value].to_i
-    vote_on_target current_user, @parent, value
+    vote_on_target current_user, @voteable, value
 
     if params[:goto].nil?
       redirect_back fallback_location: root_path
     else
-      redirect_to "#{params[:goto]}##{@parent.id}"
+      redirect_to "#{params[:goto]}##{@voteable.id}"
     end
   end
 
   private
 
-  def find_parent
-    params.each do |name, value|
-      if name =~ /(.+)_id$/
-        @parent = $1.classify.constantize.find(value)
-      end
-    end
+  def find_votable
+    model = [Post].detect { |c| params["#{c.name.underscore}_id"] }
+    @voteable = model.find(params["#{model.name.underscore}_id"])
   end
 end
