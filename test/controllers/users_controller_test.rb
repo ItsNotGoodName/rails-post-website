09ccb2aaf9543c0b464e0a5c976ae92831b05ee9
ItsNotGoodName <gurnaindeol@gmail.com>
2020-11-29 16:57:03 -0800

    Add register test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Register"
   end
+
+  test "should create user and login" do
+    post register_path, params: {user: {username: "test", password: "123456", password_confirmation: "123456"}}
+    assert_response :success
+    assert [:user_id]
+  end
 end
